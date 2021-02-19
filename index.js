b0e0779a883dc25cb7131e4d299a3f723d729839
Alisson Peloso <alisson.luan2000@gmail.com>
2021-02-19 11:55:22 -0300

    test !beep

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ const config = require("./config.json"); //Pegando o prefixo do bot para respost
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
 
 client.on('message', message => {
-  console.log(message.content);
-  if(message.content === config.prefix+'ping'){
-    message.channel.send('Pong.');
-  }
 });
+
+if(message.content === config.prefix+'ping'){
+  message.channel.send('pong.');
+}
+else if(message.content === config.prefix+'beep'){
+  message.channel.send('boop.')
+}
