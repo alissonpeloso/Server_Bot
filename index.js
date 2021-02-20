0e77b8cfa98027fb6e13adedc7fa71546d433f98
Alisson Peloso <alisson.luan2000@gmail.com>
2021-02-19 22:21:07 -0300

    playing xesquedele

--- index.js
+++ index.js
@@ -14,22 +14,27 @@ const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 
-// client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
-client.login(token); //Ligando o Bot caso ele consiga acessar o token
+client.on("ready", () => {
+  client.user.setActivity("Xesquedele | !help");
+});
 
-client.on('message', message => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+client.on('message', async message => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot || message.channel.type === "dm") return;
 
-  const args = message.content.slice(config.prefix.length).trim().split(' ');
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   if(command === 'ping'){
-    return message.channel.send('pong.');
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latência: ${m.createdTimestamp - message.createdTimestamp}ms.`);
   }
   else if (command === 'server') {
-    return message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
   }
   else if(command === 'zap'){
-    return message.channel.send(`Chama lá ${message.author} ;), 984316738`);
+    message.channel.send(`Chama lá ${message.author} ;), 984316738`);
   }
 });
+
+// client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
+client.login(token); //Ligando o Bot caso ele consiga acessar o token
