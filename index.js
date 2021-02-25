787afb48c869429bc2a1aedabb631924d382e0f2
Alisson Peloso <alisson.luan2000@gmail.com>
2021-02-24 21:52:24 -0300

    name and console update. working in host and localhos

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const express = require('express');
-const app = express();
 const token = 'ODEyMTE1NDI2Njc4MDEzOTc3.YC8Dbg.tAHOvv1LVNVQbye1GkTbrLGf26o';
-
-app.get("/", (request, response) => {
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT); // Recebe solicitações que o deixa online
-
 const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 
 client.on("ready", () => {
   client.user.setActivity("Xesquedele | !help");
+  console.log("Ourbot está pronto para uso!");
 });
 
 client.on('message', async message => {
