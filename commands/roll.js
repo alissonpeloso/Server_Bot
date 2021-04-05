9fae019a10fcfabb7fafd2cfdd0e47f3ab78e06e
Rafael Fernandes <51202489+rflgf@users.noreply.github.com>
2021-04-05 18:20:28 -0300

    Adicionando comando de rolagem de dados

---
+++ commands/roll.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async(client, message, args) => {
+	var numMaxDado = args[0];
+	var numFinal = "";
+
+	if(numMaxDado.startsWith("d"))
+		numMaxDado = numMaxDado.slice(1);
+	
+	for(var i = 1; i < args.length; i++)
+		numFinal += args[i];
+
+	numFinal = eval(numFinal);
+	
+	var randNum = Math.floor(Math.random() * parseInt(numMaxDado, 10));
+	randNum++;
+
+	numFinal += randNum;
+
+	if(randNum == numMaxDado)
+		message.channel.send(numFinal + " (" + randNum + " natural :D)");
+	else if(randNum == 1)
+		message.channel.send(numFinal + " (" + randNum + " natural D:)");
+	else
+		message.channel.send(numFinal);
+};
