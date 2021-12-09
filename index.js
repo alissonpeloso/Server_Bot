978d1292a17a575c6e7c96e64e3cbe26d29095d7
Alisson <alisson.luan2000@gmail.com>
2021-12-09 11:13:57 -0300

    fix drop server

--- index.js
+++ index.js
@@ -12,43 +12,46 @@ app = express();
 var users = ['<@!340640482256224259>', '<@!251453510296666113>', '<@!140525920111820800>', '<@!523536956576104478>', '<@!187703124402634752>'];
 
 cron.schedule('0 6 * * *', async function () {
-  for (let user of users) {
-    client.channels.cache.get("814237038680735806").send(`!sched ${user}`);
-    await new Promise(resolve => setTimeout(resolve, 5000));
-  }
-  client.channels.cache.get("812100021104148490").send("!list");
+	for (let user of users) {
+		client.channels.cache.get("814237038680735806").send(`!sched ${user}`);
+		await new Promise(resolve => setTimeout(resolve, 5000));
+	}
+	client.channels.cache.get("812100021104148490").send("!list");
 }, {
-  timezone: "America/Sao_Paulo"
+	timezone: "America/Sao_Paulo"
 });
 
 app.listen(3001);
 
 client.on("ready", () => {
-  client.user.setActivity("Xesquedele | !help");
-  console.log("Jureide está pronta para uso!");
+	client.user.setActivity("Xesquedele | !help");
+	console.log("Jureide está pronta para uso!");
 });
 
 client.on('message', async message => {
-  if (!message.content.startsWith(config.prefix) || message.channel.type === "dm") return;
+	if (!message.content.startsWith(config.prefix) || message.channel.type === "dm") return;
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
 
-  var commandSheet = await (await fetch(process.env.COMMANDS_SHEET_URL + command)).json();
+	try {
+		var commandSheet = await (await fetch(process.env.COMMANDS_SHEET_URL + command)).json();
 
-  if (!commandSheet.error) {
-    var drawn = Math.floor(Math.random() * commandSheet.length);
-    await message.channel.send(commandSheet[drawn].answer);
-  } else {
-    try {
-      let commandFile = require(`./commands/${command}.js`);
-      delete require.cache[require.resolve(`./commands/${command}.js`)];
-      return commandFile.run(client, message, args);
-    } catch (error) {
-      console.error("Erro: "+error);
-    }
-  }
-
+		if (!commandSheet.error) {
+			var drawn = Math.floor(Math.random() * commandSheet.length);
+			await message.channel.send(commandSheet[drawn].answer);
+		} else {
+			try {
+				let commandFile = require(`./commands/${command}.js`);
+				delete require.cache[require.resolve(`./commands/${command}.js`)];
+				return commandFile.run(client, message, args);
+			} catch (error) {
+				console.error("Erro: " + error);
+			}
+		}
+	} catch (error) {
+		console.error("Erro: " + error);
+	}
 });
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
