bc306ffe97541ff542807e16daf10ef27176a028
Alisson <alisson.luan2000@gmail.com>
2022-02-22 07:58:43 -0300

    fix: vercel.api not working, using opensheet.elk.sh

--- commands/sched.js
+++ commands/sched.js
@@ -7,7 +7,7 @@ async function getData(url, id) {
 
 module.exports.run = async (client, message, args) => {
 	try {
-		var sheetUrl = "https://opensheet.vercel.app/1q0LyS1weVdLfzAA7jUyF71LBLYC-X36LszYWx8LOR4s/";
+		var sheetUrl = "https://opensheet.elk.sh/1q0LyS1weVdLfzAA7jUyF71LBLYC-X36LszYWx8LOR4s/";
 		var author;
 
 		var dayArg = args[1];
@@ -36,60 +36,53 @@ module.exports.run = async (client, message, args) => {
 					break;
 			}
 		}
-	
+
 		now = new Date;
 		weekDay = now.getDay();
-	
-		if(dayArg === "tomorrow" || dayArg === "tom"){
-			weekDay = weekDay+1;
-			if(weekDay == 7){
+
+		if (dayArg === "tomorrow" || dayArg === "tom") {
+			weekDay = weekDay + 1;
+			if (weekDay == 7) {
 				weekDay = 0;
 			}
-		}
-		else if(dayArg === "monday" || dayArg === "mon"){
+		} else if (dayArg === "monday" || dayArg === "mon") {
 			weekDay = 1;
-		}
-		else if(dayArg === "tuesday" || dayArg === "tue"){
+		} else if (dayArg === "tuesday" || dayArg === "tue") {
 			weekDay = 2;
-		}
-		else if(dayArg === "wednesday" || dayArg === "wed"){
+		} else if (dayArg === "wednesday" || dayArg === "wed") {
 			weekDay = 3;
-		}
-		else if(dayArg === "thursday" || dayArg === "thu"){
+		} else if (dayArg === "thursday" || dayArg === "thu") {
 			weekDay = 4;
-		}
-		else if(dayArg === "friday" || dayArg === "fri"){
+		} else if (dayArg === "friday" || dayArg === "fri") {
 			weekDay = 5;
-		}
-		else if(dayArg === "saturday" || dayArg === "sat"){
+		} else if (dayArg === "saturday" || dayArg === "sat") {
 			weekDay = 6;
-		}
-		else if(dayArg === "sunday" || dayArg === "sun"){
+		} else if (dayArg === "sunday" || dayArg === "sun") {
 			weekDay = 0;
 		}
-	
+
 		if (!author) {
 			return;
 		}
 
 		var sched = await getData(sheetUrl, author);
 		var newMessage = "";
-	
+
 		var days = Object.keys(sched[0])
 		var day = days[weekDay + 1];
-	
+
 		for (var line of sched) {
 			if (line[day]) {
 				newMessage += `**${line["Horário"]}**: ${line[day]}\n`;
 			}
 		}
-		
+
 		const messageEmbed = new Discord.MessageEmbed()
 			.setColor('#036699')
 			.setTitle(`**${day}**`)
 			.setDescription(`${newMessage}`)
 			.setTimestamp()
-		
+
 		message.channel.send(messageEmbed);
 	} catch (error) {
 		console.log(error);
